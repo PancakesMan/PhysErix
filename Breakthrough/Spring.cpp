c8b1553f706894d4241b11af1eaa33a8867f23b0
ProProgrammerEric <eric_work@y7mail.com>
2018-02-28 11:42:29 +1030

    spring fix attempt

--- Breakthrough/Spring.cpp
+++ Breakthrough/Spring.cpp
@@ -27,8 +27,18 @@ void Spring::fixedUpdate(glm::vec2 gravity, float timeStep)
 
 	glm::vec2 force = dist * m_springCoefficient * (m_restLength - length) - m_damping * relativeVelocity;
 
-	m_body1->applyForce(-force * timeStep, p1 - m_body1->getPosition());
-	m_body2->applyForce(force * timeStep, p2 - m_body2->getPosition());
+	if (m_body1->isKinematic() && !m_body2->isKinematic()) {
+		m_body2->applyForce(force * 2.f * timeStep, p2 - m_body2->getPosition());
+		//m_body1->applyForce(-force * timeStep, p1 - m_body1->getPosition());
+	}
+	else if (m_body2->isKinematic() && !m_body1->isKinematic())
+		m_body1->applyForce(-force* 2.f * timeStep, p1 - m_body1->getPosition());
+		//m_body2->applyForce(force * timeStep, p2 - m_body2->getPosition());
+	else if (!m_body1->isKinematic() && !m_body2->isKinematic())
+	{
+		m_body1->applyForce(-force * timeStep, p1 - m_body1->getPosition());
+		m_body2->applyForce(force * timeStep, p2 - m_body2->getPosition());
+	}
 }
 
 void Spring::draw(aie::Renderer2D* renderer)
